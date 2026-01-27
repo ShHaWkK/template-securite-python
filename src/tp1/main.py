7016132a9b60dab2d8d91e4c03f55befb596e98c
ShHaWkK <51519814+ShHaWkK@users.noreply.github.com>
2026-01-27 18:47:03 +0100

    Change traffic analysis from 'tcp' to 'all'

--- src/tp1/main.py
+++ src/tp1/main.py
@@ -8,7 +8,7 @@ def main():
 
     capture = Capture()
     capture.capture_traffic()
-    capture.analyse("tcp")
+    capture.analyse("all")
     summary = capture.get_summary()
 
     filename = "report.pdf"
